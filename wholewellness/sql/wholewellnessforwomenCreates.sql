1dc3408115c13f4efb1559b847e06c0a8b31ade3
gdc24 <gdc24@case.edu>
2020-03-22 21:12:36 -0400

    create scripts
    to be run in pgadmin 4

---
+++ wholewellness/sql/wholewellnessforwomenCreates.sql
@@ -0,0 +1,151 @@
+CREATE TYPE mealType AS ENUM (
+  'breakfast',
+  'lunch',
+  'dinner',
+  'snack'
+);
+
+CREATE TYPE intensity AS ENUM (
+  'low',
+  'med',
+  'high'
+);
+
+CREATE TYPE muscleGroup AS ENUM (
+  'arms',
+  'abs',
+  'butt',
+  'legs'
+);
+
+CREATE TYPE equipment AS ENUM (
+  'none',
+  'dumbbells',
+  'treadmill',
+  'elliptical',
+  'exerciseBall',
+  'foamRoller',
+  'stationaryBike',
+  'jumpRope'
+);
+
+CREATE TABLE "user" (
+  "intUserID" SERIAL PRIMARY KEY,
+  "strUsername" varchar UNIQUE NOT NULL,
+  "weight" int NOT NULL,
+  "heightInInches" int NOT NULL,
+  "exerciseLevel" intensity NOT NULL,
+  "allotedCalorites" int NOT NULL,
+  "allotedExerciseMinutes" int NOT NULL
+);
+
+CREATE TABLE "day" (
+  "intDayID" SERIAL PRIMARY KEY,
+  "dtm" date NOT NULL,
+  "intCalsLeft" int NOT NULL,
+  "intUserID" int NOT NULL
+);
+
+CREATE TABLE "dayMeal" (
+  "intDayMealID" SERIAL PRIMARY KEY,
+  "intDayID" int NOT NULL,
+  "intMealID" int NOT NULL
+);
+
+CREATE TABLE "dayExercise" (
+  "intDayExerciseID" SERIAL PRIMARY KEY,
+  "intDayID" int,
+  "intWorkoutRoutineID" int NOT NULL
+);
+
+CREATE TABLE "foodItem" (
+  "intFoodItemID" SERIAL PRIMARY KEY,
+  "strName" varchar NOT NULL,
+  "intCalories" int NOT NULL,
+  "strBrandName" varchar
+);
+
+CREATE TABLE "meal" (
+  "intMealID" SERIAL PRIMARY KEY,
+  "mealType" mealType NOT NULL,
+  "intUserID" int NOT NULL
+);
+
+CREATE TABLE "favoritesFood" (
+  "intFavoritesFoodID" SERIAL PRIMARY KEY,
+  "intUserID" int NOT NULL,
+  "intFoodItemID" int NOT NULL
+);
+
+CREATE TABLE "healthierOptions" (
+  "intHealthierOptionID" SERIAL PRIMARY KEY,
+  "intOriginalFoodItemID" int NOT NULL,
+  "intAlternativeFoodItemID" int NOT NULL
+);
+
+CREATE TABLE "exerciseType" (
+  "intExerciseTypeID" SERIAL PRIMARY KEY,
+  "muscleGroup" muscleGroup NOT NULL,
+  "equipment" equipment NOT NULL,
+  "intCaloriesBurned" int NOT NULL,
+  "ysnAccessibility" boolean NOT NULL,
+  "intTime" int NOT NULL,
+  "intensity" intensity NOT NULL
+);
+
+CREATE TABLE "favortiesExercise" (
+  "intExerciseFavortiesID" SERIAL PRIMARY KEY,
+  "intUserID" int NOT NULL,
+  "intExerciseTypeID" int NOT NULL
+);
+
+CREATE TABLE "muscleExercise" (
+  "intMuscleExerciseID" SERIAL PRIMARY KEY,
+  "muscleGroup" muscleGroup NOT NULL,
+  "intExerciseTypeID" int NOT NULL
+);
+
+CREATE TABLE "workoutRoutine" (
+  "intWorkoutRoutineID" SERIAL PRIMARY KEY,
+  "intTotalMinutes" int NOT NULL,
+  "intTotalCalsBurned" int NOT NULL,
+  "intUserID" int NOT NULL
+);
+
+CREATE TABLE "workoutExerciseType" (
+  "intWorkoutExerciseType" SERIAL PRIMARY KEY,
+  "intExerciseTypeID" int NOT NULL,
+  "intWorkoutRoutineID" int NOT NULL
+);
+
+ALTER TABLE "day" ADD FOREIGN KEY ("intUserID") REFERENCES "user" ("intUserID");
+
+ALTER TABLE "dayMeal" ADD FOREIGN KEY ("intDayID") REFERENCES "day" ("intDayID");
+
+ALTER TABLE "dayMeal" ADD FOREIGN KEY ("intMealID") REFERENCES "meal" ("intMealID");
+
+ALTER TABLE "dayExercise" ADD FOREIGN KEY ("intDayID") REFERENCES "day" ("intDayID");
+
+ALTER TABLE "dayExercise" ADD FOREIGN KEY ("intWorkoutRoutineID") REFERENCES "workoutRoutine" ("intWorkoutRoutineID");
+
+ALTER TABLE "meal" ADD FOREIGN KEY ("intUserID") REFERENCES "user" ("intUserID");
+
+ALTER TABLE "favoritesFood" ADD FOREIGN KEY ("intUserID") REFERENCES "user" ("intUserID");
+
+ALTER TABLE "favoritesFood" ADD FOREIGN KEY ("intFoodItemID") REFERENCES "foodItem" ("intFoodItemID");
+
+ALTER TABLE "healthierOptions" ADD FOREIGN KEY ("intOriginalFoodItemID") REFERENCES "foodItem" ("intFoodItemID");
+
+ALTER TABLE "healthierOptions" ADD FOREIGN KEY ("intAlternativeFoodItemID") REFERENCES "foodItem" ("intFoodItemID");
+
+ALTER TABLE "favortiesExercise" ADD FOREIGN KEY ("intUserID") REFERENCES "user" ("intUserID");
+
+ALTER TABLE "favortiesExercise" ADD FOREIGN KEY ("intExerciseTypeID") REFERENCES "exerciseType" ("intExerciseTypeID");
+
+ALTER TABLE "muscleExercise" ADD FOREIGN KEY ("intExerciseTypeID") REFERENCES "exerciseType" ("intExerciseTypeID");
+
+ALTER TABLE "workoutRoutine" ADD FOREIGN KEY ("intUserID") REFERENCES "user" ("intUserID");
+
+ALTER TABLE "workoutExerciseType" ADD FOREIGN KEY ("intExerciseTypeID") REFERENCES "exerciseType" ("intExerciseTypeID");
+
+ALTER TABLE "workoutExerciseType" ADD FOREIGN KEY ("intWorkoutRoutineID") REFERENCES "workoutRoutine" ("intWorkoutRoutineID");
