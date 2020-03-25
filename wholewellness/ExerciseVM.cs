5ccb1194116e938901a9a844edc4f91a668972a6
allydinhofer <55101375+allydinhofer@users.noreply.github.com>
2020-03-24 21:50:20 -0400

    trying to do VMs

---
+++ wholewellness/ExerciseVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using wholewellness.Models;
+namespace wholewellness.Views.ViewModels.CRUD_VMs
+{
+    public class ExerciseVM : WorkoutRoutine
+    {
+        public IEnumerable<WorkoutRoutine> LstWorkoutRoutines { get; set; }
+    }
+}
