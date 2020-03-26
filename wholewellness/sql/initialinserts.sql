411372e95a317f207e0d7e60945ca13feab6b86d
gdc24 <gdc24@case.edu>
2020-03-26 12:22:06 -0400

    add insert to food items

--- wholewellness/sql/initialinserts.sql
+++ wholewellness/sql/initialinserts.sql
@@ -8,3 +8,17 @@ INSERT INTO public.day(
 	VALUES (CURRENT_DATE, (SELECT "intAllotedCalories" FROM "user" WHERE "intUserID" = 1), 1),
 	(CURRENT_DATE, (SELECT "intAllotedCalories" FROM "user" WHERE "intUserID" = 2), 2);
 
+INSERT INTO public."foodItem"(
+	"strName", "intCalories", "strBrandName")
+	VALUES ('plain bagel', 288, null),
+	('Italian bread', 90, 'Pepperidge Farm'),
+	('English muffin', 150, 'Thomas'''),
+	('apple', 95, null),
+	('banana', 105, null),
+	('classic potato chips', 160, 'Lay''s'),
+	('chocolate chip cookie', 78, null),
+	('American cheese slice', 50, 'Kraft'),
+	('mayonnaise', 90, 'Hellman''s'),
+	('light mayonnaise', 35, 'Hellman''s'),
+	('strawberry yogurt', 150, 'Yoplait'),
+	('raspberries', 65, null);
