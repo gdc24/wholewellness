a6722d505e37f7ac74e3c7265e1b2a606810b751
gdc24 <gdc24@case.edu>
2020-04-13 18:22:33 -0400

    update scripts

--- wholewellness/sql/random scripts.sql
+++ wholewellness/sql/random scripts.sql
@@ -63,3 +63,41 @@ INSERT INTO public."exerciseType"(
 SELECT unnest(enum_range(NULL::equipment))
 SELECT unnest(enum_range(NULL::muscleGroup))
 SELECT unnest(enum_range(NULL::intensity))
+
+
+
+SELECT * FROM "workoutRoutine"
+WHERE "muscleGroup" = 'arms'
+SELECT * FROM (SELECT * FROM "exerciseType"
+			   WHERE "muscleGroup" = 'arms') initialSearch
+WHERE "muscleGroup" = 'arms'
+  AND "equipment" = 'dumbbells'
+  AND "intensity" = 'low'
+  AND "ysnAccessibility" = false
+  
+  SELECT * FROM "day"
+  SELECT * FROM "user"
+  SELECT wr."intWorkoutRoutineID",
+  		 wr."intTotalMinutes",
+		 wr."intTotalCalsBurned",
+		 wr."intUserID"
+   FROM "workoutRoutine" wr, "day" d, "dayExercise" de
+   WHERE wr."intUserID" = 1
+	AND d."intDayID" = 9324
+	AND de."intDayID" = d."intDayID"
+	AND wr."intWorkoutRoutineID" = de."intWorkoutRoutineID"
+  
+  --ALTER TABLE "day" ALTER COLUMN intExMinsLeft SET NOT NULL;
+  --ALTER TABLE "day" RENAME COLUMN intExMinsLeft TO "intExMinsLeft";
+  --ALTER TABLE "day" ADD COLUMN allotedExerciseMins int;
+  
+
+SELECT * FROM "exerciseType"
+SELECT unnest(enum_range(NULL::equipment))
+SELECT unnest(enum_range(NULL::muscleGroup))
+SELECT unnest(enum_range(NULL::intensity))
+
+SELECT * FROM "day" WHERE "intUserID" = 1 AND "dtmDate" = '2020-04-12'
+SELECT * FROM "user"
+(select(current_date at time zone '-4')::date)
+SELECT CURRENT_DATE
